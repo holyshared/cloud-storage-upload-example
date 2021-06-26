55832e8b2476629fcee60f91c80fa627dae3d47d
holyshared <holy.shared.design@gmail.com>
2021-06-27 00:27:19 +0900

    add storage

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import { Storage } from "@google-cloud/storage";
-import dayjs from "dayjs";
-
-const SIGNED_EXPIRES = Number(process.env.SIGNED_EXPIRES || 5);
-
-const storage = new Storage({});
+import { signedUrl } from "./storage";
 
 const app = express();
 
@@ -14,22 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post("/signed", (req: Request<{}, {}, { name: string }>, res: Response, next: NextFunction) => {
-  const options = {
-    userProject: process.env.GCP_PROJECT_ID
-  };
-
-  const expires = dayjs().add(SIGNED_EXPIRES, 'minute').toDate()
-
-  storage.bucket(process.env.GCP_STORAGE_BUCKET).file(req.body.name, options).getSignedUrl({
-    action: 'write',
-    expires,
-  }, (err: Error, url: string) => {
-    if (err) {
-      return next(err);
-    }
-    res.json({
-      url
-    });
+  signedUrl(req.body.name).then((result) => {
+    res.json(result);
+  }).catch(err => {
+    next(err);
   });
 });
 
---
+++ src/storage.ts
@@ -0,0 +1,29 @@
+import { Storage } from "@google-cloud/storage";
+import dayjs from "dayjs";
+
+const SIGNED_EXPIRES = Number(process.env.SIGNED_EXPIRES || 5);
+
+const storage = new Storage({
+  projectId: process.env.GCP_PROJECT_ID
+});
+
+export const signedUrl = async (name: string): Promise<{
+  url: string
+}> => {
+  const options = {};
+  const expires = dayjs().add(SIGNED_EXPIRES, 'minute').toDate();
+
+  return new Promise((resolve, reject) => {
+    return storage.bucket(process.env.GCP_STORAGE_BUCKET).file(name, options).getSignedUrl({
+      action: 'write',
+      expires,
+    }, (err: Error, url: string) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve({
+        url
+      });
+    });
+  });
+}
