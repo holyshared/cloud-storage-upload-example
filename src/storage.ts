2f667654d4472eda54d9abceeb5840f5c9039766
holyshared <holy.shared.design@gmail.com>
2021-06-27 19:36:59 +0900

    fix signed url

--- src/storage.ts
+++ src/storage.ts
@@ -1,7 +1,4 @@
 import { Storage } from "@google-cloud/storage";
-import dayjs from "dayjs";
-
-const SIGNED_EXPIRES = Number(process.env.SIGNED_EXPIRES || 5);
 
 const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID
@@ -12,13 +9,13 @@ export const signedUrl = async (name: string): Promise<{
 }> => {
   const options = {
   };
-  const expires = dayjs().add(SIGNED_EXPIRES, 'minute').toDate();
 
   return new Promise((resolve, reject) => {
     return storage.bucket(process.env.GCP_STORAGE_BUCKET).file(name, options).getSignedUrl({
       version: 'v4',
       action: 'write',
-      expires,
+      expires: Date.now() + 15 * 60 * 1000,
+      contentType: "image/jpeg"
     }, (err: Error, url: string) => {
       if (err) {
         return reject(err);
