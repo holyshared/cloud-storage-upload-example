8246cd4930e104f08b77a934ad259a1fe3114098
holyshared <holy.shared.design@gmail.com>
2021-07-11 17:49:43 +0900

    cleanup base64

--- src/signed-cookie.ts
+++ src/signed-cookie.ts
@@ -8,12 +8,19 @@ export const SIGNED_URL_MAX_AGE_SECONDS = Number(process.env.SIGNED_URL_MAX_AGE_
 export const SIGNED_URL_DOMAIN = process.env.SIGNED_URL_DOMAIN;
 export const SIGNED_URL_PATH = process.env.SIGNED_URL_PATH;
 
-export const ENCODED_URL_PREFIX = (Buffer.from(`https://${SIGNED_URL_DOMAIN}${SIGNED_URL_PATH}`)).toString("base64");
+export const ENCODED_URL_PREFIX = (Buffer.from(`https://${SIGNED_URL_DOMAIN}${SIGNED_URL_PATH}`))
+  .toString("base64")
+  .replace(/\+/g, "-")
+  .replace(/\//g, "_");
 
 export const signedCookie = (seconds: number) => {
   const unixTimestamp = dayjs().add(seconds, 'seconds').unix();
   const input = `URLPrefix=${ENCODED_URL_PREFIX}:Expires=${unixTimestamp}:KeyName=${SIGNED_URL_KEY_NAME}`;
-  const signature =  crypto.createHmac('sha1', SIGNED_URL_KEY_VALUE).update(input).digest('base64');
+  const signature =  crypto.createHmac('sha1', SIGNED_URL_KEY_VALUE)
+    .update(input)
+    .digest('base64')
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
 
   return `${input}:Signature=${signature}`;
 }
