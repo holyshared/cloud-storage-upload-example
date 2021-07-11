4640420ec967036752ff17ff63ef8598ac7a2fbc
holyshared <holy.shared.design@gmail.com>
2021-07-11 16:40:13 +0900

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,10 @@
 import typescript from '@rollup/plugin-typescript';
-//import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: './src/client.ts',
   plugins: [
     commonjs(),
-//    babel({
-  //    babelHelpers: 'bundled',
-    //  exclude: ["node_modules"]
-//    }),
     typescript({
       module: "esnext",
       resolveJsonModule: false,
